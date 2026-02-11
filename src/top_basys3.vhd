3b49953eb15d5d348dafe84170e99f94dda85205
jonathanhammond2 <82597323+jonathanhammond2@users.noreply.github.com>
2026-02-11 16:11:03 -0700

    .

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -56,6 +56,15 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
 	
     -- declare the component of your top-level design
+    component ripple_adder is
+       Port ( A : in STD_LOGIC_VECTOR (3 downto 0);
+           B : in STD_LOGIC_VECTOR (3 downto 0);
+           Cin : in STD_LOGIC;
+           S : out STD_LOGIC_VECTOR (3 downto 0);
+           Cout : out STD_LOGIC);
+        end component ripple_adder;
+    
+    
 
     -- declare any signals you will need	
   
@@ -63,6 +72,15 @@ begin
 	-- PORT MAPS --------------------
    
 	---------------------------------
+	ripple_adder_0: ripple_adder
+    port map(
+        A     => sw(4 downto 1),
+        B     => sw(15 downto 12),
+        Cin   => sw(0),   -- Directly to input here
+        S     => led(3 downto 0),
+        Cout  => led(15)
+    );
+	
 	
 	-- CONCURRENT STATEMENTS --------
 	led(14 downto 4) <= (others => '0'); -- Ground unused LEDs
